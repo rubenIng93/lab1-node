7ce5ab0ffee8ba72f8579e53db747bdbab6e38d3
ruben <s236819@studenti.polito.it>
2021-03-15 16:06:20 +0100

    Lab 1 finished, probably some improvements are possible

--- Lab1_ex1.js
+++ Lab1_ex1.js
@@ -1,6 +1,11 @@
 "use strict";
 
 const dayjs = require('dayjs');
+const sqlite = require('sqlite3');
+
+// open the db
+const db = new sqlite.Database('tasks.db',
+    (err) => {if(err) throw err;})
 
 
 // define an object Task
@@ -59,17 +64,124 @@ const t1 = new Task(3, 'phone call', true, false, 'March 8, 2021 4:20 PM');
 
 
 let taskList = new TaskList();
-taskList.addTask(t2);
-taskList.addTask(t3);
-taskList.addTask(t1);
+//taskList.addTask(t2);
+//taskList.addTask(t3);
+//taskList.addTask(t1);
 
 
 //console.log(t1.toString());
 console.log(taskList.toString());
-console.log('\n'+'******' + 'Task sorted by deadline (most recent first):******')
-taskList.sortAndPrint();
-console.log('\n'+'******' + 'Task filtered, only (urgent == true):******')
-taskList.filterAndPrint();
+//console.log('\n'+'******' + 'Task sorted by deadline (most recent first):******')
+//taskList.sortAndPrint();
+//console.log('\n'+'******' + 'Task filtered, only (urgent == true):******')
+//taskList.filterAndPrint();
 
 
-//console.log(taskList.toString());
+//load all the dbs task in the list
+
+function insertAllFromDb() {
+
+    const sql = 'SELECT * FROM tasks';
+
+    return new Promise( (resolve, reject) => {
+        db.all(sql, (err, rows) => {
+            if (err) {
+                reject(err);
+            }               
+            else {
+                for (let row of rows){
+                    let task = new Task(
+                        row.id,
+                        row.description,
+                        row.urgent,
+                        row.private,
+                        row.deadline
+                    );
+                    taskList.addTask(task);
+                }
+
+                resolve(taskList);
+            }
+        })
+    })
+}
+
+function searchByDeadline(deadline){
+    const sql = "SELECT * FROM tasks WHERE deadline > ?";
+
+    return new Promise( (resolve, reject) => {
+        db.all(sql, [deadline], (err, rows) => {
+            if (err) {
+                reject(err);
+            }               
+            else {
+
+                let list = new TaskList();
+
+                for (let row of rows){
+                    let task = new Task(
+                        row.id,
+                        row.description,
+                        row.urgent,
+                        row.private,
+                        row.deadline
+                    );
+                    list.addTask(task);
+                }
+
+                resolve(list);
+            }
+        })
+    })    
+}
+
+
+function searchByKeyword(keyword){
+    let likeKeyword = '%'+keyword+'%';
+    const sql = "SELECT * FROM tasks WHERE description LIKE ?";
+
+    return new Promise( (resolve, reject) => {
+        db.all(sql, [likeKeyword], (err, rows) => {
+            if (err) {
+                reject(err);
+            }               
+            else {
+
+                let list = new TaskList();
+
+                for (let row of rows){
+                    let task = new Task(
+                        row.id,
+                        row.description,
+                        row.urgent,
+                        row.private,
+                        row.deadline
+                    );
+                    list.addTask(task);
+                }
+
+                resolve(list);
+            }
+        })
+    })  
+
+}
+
+async function main(){
+    await insertAllFromDb();
+    console.log('*** Tasks inserted ***');
+    console.log(taskList.toString());
+    let deadlineList = await searchByDeadline('2021-03-10');
+    console.log('\n**** Tasks after '+dayjs('2021-03-10').format('MMMM D, YYYY h:mm A')+' ****')
+    console.log(deadlineList.toString());
+    let wordList = await searchByKeyword('lab');
+    console.log('\n**** Tasks which contains the word "lab":****')
+    console.log(wordList.toString());
+}
+
+main();
+
+
+
+
+
